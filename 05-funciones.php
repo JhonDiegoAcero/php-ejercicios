fa4bbca21d439ab4ee40800867d906ffdf142de8
Belinda Arocutipa <1478562@SENATI.PE>
2024-05-15 17:36:05 -0500

    nuevo ejercicio

--- 05-funciones.php
+++ 05-funciones.php
@@ -10,10 +10,21 @@ function mostrarDatos($nombres,$apellidos,$curso,$semestre){
     return $mensaje;
 }
 
-
+function calcular($operador,$numero1,$numero2) {
+    if($operador=="+")
+    return $numero1+$numero2;
+    if($operador=="-")
+    return $numero1+$numero2;
+    if($operador=="*")
+    return $numero1+$numero2;
+    if($operador=="/")
+    return $numero1+$numero2;
+}
 
 
 echo obtenerSaludo("Jhon")."\n";
 echo mostrarDatos("Jhon","Acero",
 "backend developer web","tercer semestre");
+echo "El total de la suma es: ".calcular("+",10,34);
+
 ?>
