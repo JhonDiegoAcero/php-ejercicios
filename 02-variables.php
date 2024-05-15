c16ad770db6dc9d01dd3dcee4b0724a55a30e618
Belinda Arocutipa <1478562@SENATI.PE>
2024-05-15 16:43:38 -0500

    nuevos ejercicios

---
+++ 02-variables.php
@@ -0,0 +1,10 @@
+<?php
+
+$nombres ="Jhon";
+$apellidos ="Acero";
+$curso ="BACKEND DEVELOPER WEB";
+$semestre = "Tercer semestre";
+
+echo $nombres." ".$apellidos." ".$curso." ".$semestre ;
+
+?>
---
+++ 04-cadenas.php
@@ -0,0 +1,10 @@
+<?php
+    $cadena="El curso ha terminado";
+    echo "<h1>";
+    echo "El texto en mayuscala". strtoupper($cadena)."<br>";
+    echo "El texto en minuscula". strtoupper($cadena)."<br>";
+    echo "El texto tiene ".strlen($cadena)." caracteres <br>";
+    echo "El texto dice ahora ".
+    str_replace("terminado","finalizado",$cadena)."<br>";
+    echo "</h1>";
+?>
